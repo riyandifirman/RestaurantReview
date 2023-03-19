61090604e374ca7bff0d9f8b30c5a084d5d13f16
riyandifirman <riyandi.firman08@gmail.com>
2023-03-19 14:49:50 +0700

    change livedata with Single Event

--- app/src/main/java/com/riyandifirman/restaurantreview/MainActivity.kt
+++ app/src/main/java/com/riyandifirman/restaurantreview/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
 import com.riyandifirman.restaurantreview.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -42,6 +43,17 @@ class MainActivity : AppCompatActivity() {
         })
         mainViewModel.isLoading.observe(this, {
             showLoading(it)
+        })
+
+
+        mainViewModel.snackbarText.observe(this, {
+            it.getContentIfNotHandled()?.let { snackBarText ->
+                Snackbar.make(
+                    window.decorView.rootView,
+                    snackBarText,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
         })
 
         binding.btnSend.setOnClickListener { view ->
